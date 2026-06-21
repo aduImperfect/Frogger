ffc46a7c0c92197fc7c00ca77cd85323da7f8615
Aditya Subramanian <adu101992@gmail.com>
2026-06-21 22:08:27 +0530

    - Added in wood and water for the upper section. - Added in camera switching. - Updated the goal post to only trigger win condition on reaching past the water body. - Updated movement logic to function properly based on which yDepth the frog is at.

--- frogger/TSCN/player_movement.gd
+++ frogger/TSCN/player_movement.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var sceneGen : Node2D
+@export var camera : Camera2D
 
 @export var frogVertSpeed : float = 0.0
 @export var frogSpeedVertAdditive : float = 0.0
@@ -13,6 +14,12 @@ extends Node2D
 @export var xMoveLimit : int = 0
 @export var yMoveLimit : int = 0
 
+@export var yMoveCamUpSwitchLimit : int = 0
+@export var yMoveCamDownSwitchLimit : int = 0
+
+@export var yUpCamPosX : float = 0
+@export var yDownCamPosX : float = 0
+
 @export var gameWon : bool = false
 @export var gameLost : bool = false
 
@@ -24,10 +31,13 @@ func _ready() -> void:
 	xPath = 0
 	yLane = -1
 	xMoveLimit = 10
-	yMoveLimit = 4
+	yMoveLimit = 10
+	yMoveCamUpSwitchLimit = 5
+	yMoveCamDownSwitchLimit = 4
 	gameWon = false
 	gameLost = false
-
+	yUpCamPosX = -395.0
+	yDownCamPosX = 325.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -50,32 +60,46 @@ func _input_management(_delta : float) -> void:
 
 func _move_character(_movementStr : String) -> void:
 	if _movementStr == "up":
-		if yLane >= 0 && yLane < (yMoveLimit - 1):
+#------------------------
+		if yLane == (yMoveCamUpSwitchLimit - 1):
+			camera.global_position.y = yUpCamPosX
+			print("Cam Up Switch")
+#------------------------
+		if (yLane >= 0) && (yLane < (yMoveCamUpSwitchLimit - 2)):
+			sceneGen.frogNode.position.y -= frogSpeedVertAdditive
+		elif (yLane > yMoveCamUpSwitchLimit) && (yLane < yMoveLimit - 1):
 			sceneGen.frogNode.position.y -= frogSpeedVertAdditive
 		elif yLane == yMoveLimit:
-			print(sceneGen.frogNode.position)
 			return
 		sceneGen.frogNode.position.y -= frogVertSpeed
 		yLane += 1
 	elif _movementStr == "down":
+#------------------------
+		if yLane == (yMoveCamDownSwitchLimit + 1):
+			camera.global_position.y = yDownCamPosX
+			print("Cam Down Switch")
+#------------------------
 		if yLane == -1:
-			print(sceneGen.frogNode.position)
 			return
-		elif yLane > 0 && yLane <= 3:
+		elif (yLane > 0) && (yLane <= (yMoveCamDownSwitchLimit - 1)):
+			sceneGen.frogNode.position.y += frogSpeedVertAdditive
+		elif (yLane > yMoveCamDownSwitchLimit + 2) && (yLane < yMoveLimit):
 			sceneGen.frogNode.position.y += frogSpeedVertAdditive
 		sceneGen.frogNode.position.y += frogVertSpeed
 		yLane -= 1
+#------------------------
 	elif _movementStr == "left":
 		if xPath == -xMoveLimit:
-			print(sceneGen.frogNode.position)
 			return
 		sceneGen.frogNode.position.x -= frogHorizSpeed
 		xPath -= 1
 	elif _movementStr == "right":
 		if xPath == xMoveLimit:
-			print(sceneGen.frogNode.position)
 			return
 		sceneGen.frogNode.position.x += frogHorizSpeed
 		xPath += 1
+#------------------------
 
+	print(xPath)
+	print(yLane)
 	print(sceneGen.frogNode.position)
--- frogger/TSCN/scene_gen.gd
+++ frogger/TSCN/scene_gen.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 const ROAD_SCENE = preload("res://TSCN/road.tscn")
+const WATER_SCENE = preload("res://TSCN/water.tscn")
 const FROG_SCENE = preload("res://TSCN/frog.tscn")
 const VEHICLE_SCENE = preload("res://TSCN/vehicle.tscn")
+const WOOD_SCENE = preload("res://TSCN/wood.tscn")
 
 @export var frogNode : Node2D
 @export var frogXVal : float = 0.0
@@ -10,6 +12,9 @@ const VEHICLE_SCENE = preload("res://TSCN/vehicle.tscn")
 
 @export var vehiclesArr : Array[Node2D] = []
 @export var vehiclesNum : int = 0
+
+@export var woodsArr : Array[Node2D] = []
+@export var woodsNum : int = 0
 
 @export var infiniteXVal : float = 0.0
 @export var infiniteYVal : float = 0.0
@@ -20,8 +25,17 @@ const VEHICLE_SCENE = preload("res://TSCN/vehicle.tscn")
 @export var xRoadCount : int = 0
 @export var yRoadCount : int = 0
 
+@export var xWoodsCount : int = 0
+@export var yWoodsCount : int = 0
+
+@export var xWaterCount : int = 0
+@export var yWaterCount : int = 0
+
 @export var xBeginVal : float = 0.0
 @export var yBeginVal : float = 0.0
+
+@export var yUpperBeginVal : float = 0.0
+
 @export var xOffset : float = 0.0
 @export var yOffset : float = 0.0
 
@@ -30,20 +44,34 @@ const VEHICLE_SCENE = preload("res://TSCN/vehicle.tscn")
 func _ready() -> void:
 	frogXVal = 575.0
 	frogYVal = 630.0
-	vehiclesNum = 24
+
+	vehiclesNum = 16
+	woodsNum = 16
+
 	infiniteXVal = -9999.0
 	infiniteYVal = -9999.0
+
 	xRoadCount = 6
 	yRoadCount = 4
+
 	xVehiclesCount = 4
 	yVehiclesCount = 4
+
+	xWaterCount = 6
+	yWaterCount = 4
+
+	xWoodsCount = 4
+	yWoodsCount = 4
 	xBeginVal = 100.0
 	yBeginVal = 100.0
+	yUpperBeginVal = -620.0
 	xOffset = 200.0
 	yOffset = 145.0
 	_spawn_frog()
 	_spawn_roads()
 	_spawn_vehicles()
+	_spawn_waters()
+	_spawn_woods()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -71,6 +99,24 @@ func _spawn_vehicles() -> void:
 	for k in vehiclesNum:
 		vehicle_instance = VEHICLE_SCENE.instantiate()
 		vehicle_instance.global_position.x = infiniteXVal
-		vehicle_instance.global_position.y = infiniteXVal
+		vehicle_instance.global_position.y = infiniteYVal
 		add_child(vehicle_instance)
 		vehiclesArr.append(vehicle_instance)
+
+func _spawn_waters() -> void:
+	var water_instance
+	for j in yWaterCount:
+		for k in xWaterCount:
+			water_instance = WATER_SCENE.instantiate()
+			water_instance.global_position.x = xBeginVal + (k * xOffset)
+			water_instance.global_position.y = yUpperBeginVal + (j * yOffset)
+			add_child(water_instance)
+
+func _spawn_woods() -> void:
+	var wood_instance
+	for k in vehiclesNum:
+		wood_instance = WOOD_SCENE.instantiate()
+		wood_instance.global_position.x = infiniteXVal
+		wood_instance.global_position.y = infiniteYVal
+		add_child(wood_instance)
+		woodsArr.append(wood_instance)
